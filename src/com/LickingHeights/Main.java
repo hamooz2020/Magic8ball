8e801d078af46a060cef175ddaa3b4c39e2db720
Hamza <hamooz7670@gmail.com>
2019-09-30 09:55:10 -0400

    int stuff and made a comment

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,42 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //declare
+        Scanner keyboard;
+        String first,second,third,four,five,six,seven,eight,nine,ten;
+
+
+        //initialize
+
+
+        System.out.println("WOW! that's smart");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
