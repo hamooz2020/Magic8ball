5dd317dd6b0fd8ccead513e448e711dfb5091324
Hamza <hamooz7670@gmail.com>
2019-10-03 09:45:52 -0400

    took user input and improved my code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -10,9 +10,10 @@ public class Main {
         Scanner keyboard;
         String first,second,third,four,five,six,seven,eight,nine,ten;
 
+        keyboard = new Scanner(System.in);
 
         //initialize
-
+        int number;
 
         System.out.println("WOW! that's smart");
 
@@ -28,29 +29,39 @@ public class Main {
 
         System.out.println("Don't do it.");
 
-        System.out.println("That's not smart.");
+        String thirdResponse = "That's not smart.";
 
-        System.out.println("You shouldn't do it.");
+        String secondResponse = "You shouldn't do it.";
 
-        System.out.println("That's not a smart idea.");
-
-
+        String firstResponse = "That's not a smart idea.";
 
 
+        System.out.println("Pick a number between 1 and 100");
+        number = keyboard.nextInt();
 
+        if( number> 0 && number <=10) {
+            System.out.println(firstResponse);
+        }
 
+        if( number> 0 && number <=10)
 
+            if( number> 11 && number <=20)
 
+                if( number> 21 && number <=30)
 
+                    if( number> 31 && number <=40)
 
+                        if( number> 41 && number <=50)
 
+                            if( number> 51 && number <=60)
 
+                                if( number> 61 && number <=70)
 
+                                    if( number> 71 && number <=80)
 
+                                        if( number> 81 && number <=90)
 
-
-
-
+                                            if( number> 91 && number <=100)
 
     }
 }
