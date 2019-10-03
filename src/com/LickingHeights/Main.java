5585b77444447164582e013285bd3c930c02d2ae
Hamza <hamooz7670@gmail.com>
2019-10-03 10:26:32 -0400

    Finished code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //declare
         Scanner keyboard;
-        String first,second,third,four,five,six,seven,eight,nine,ten;
+        String first, second, third, four, five, six, seven, eight, nine, ten;
 
         keyboard = new Scanner(System.in);
 
@@ -23,7 +23,7 @@ public class Main {
 
         String seventhResponse = "That wouldn't be a bad idea!";
 
-        String sixthResponse ="Give it a try!";
+        String sixthResponse = "Give it a try!";
 
         String fifthResponse = "That's a bad idea.";
 
@@ -39,31 +39,28 @@ public class Main {
         System.out.println("Pick a number between 1 and 100");
         number = keyboard.nextInt();
 
-        if( number> 0 && number <=10) {
+        if (number > 0 && number <= 10) {
             System.out.println(firstResponse);
+        } else if (number > 11 && number <= 20) {
+            System.out.println(secondResponse);
         }
-
-        if( number> 0 && number <=10)
-
-            if( number> 11 && number <=20)
-
-                if( number> 21 && number <=30)
+        if (number > 20 && number <= 30) {
             System.out.println(thirdResponse);
-                    if( number> 31 && number <=40)
-            System.out.println(fourthResponse);
-                        if( number> 41 && number <=50)
-            System.out.println(fifthResponse);
-                            if( number> 51 && number <=60)
-            System.out.println(sixthResponse);
-                                if( number> 61 && number <=70)
-            System.out.println(seventhResponse);
-                                    if( number> 71 && number <=80)
-             System.out.println(eightResponse);
-
-                                        if( number> 81 && number <=90)
-            System.out.println(nineResponse);
-
-                                            if( number> 91 && number <=100){
+        }
+                    if( number> 30 && number <=40){
+           System.out.println(fourthResponse);}
+                       if( number> 40 && number <=50){
+            System.out.println(fifthResponse);}
+                            if( number> 50 && number <=60){
+            System.out.println(sixthResponse);}
+                                if( number> 60 && number <=70){
+            System.out.println(seventhResponse);}
+                                    if( number> 70 && number <=80){
+            System.out.println(eightResponse);}
+             if( number> 80 && number <=90) {
+                                            System.out.println(nineResponse);
+                                        }
+                                            if( number> 90 && number <=100){
             System.out.println(tenResponse);
                                             }
 
